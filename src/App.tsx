cdeba64761f40daa01353252523e5078f233c7db
bekavacKa <th.beka17@gmail.com>
2022-08-30 20:34:35 +0200

    adding todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputFeild from './components/InputFeild';
+import Todo from './components/model';
+import TodoList from './components/TodoList';
 
-function App() {
+// let name: string;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number,string];
+
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: "Kazimir",
+//   age: 27
+// }
+
+// let lotsOfPeople: Person[];
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+  // console.log(todo);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo:todo, isDone: false}])
+      setTodo("");
+    }
+
+  }
+
+  console.log(todos);
+
+  
+
   return (
     <div className="App">
-      ToDO app start
+      <span className='heading'>TASKS</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
+      {/* {todos.map((t) => (<li>{t.todo}</li>))} */}
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
